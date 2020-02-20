3e6001a79b944c1a2b78ec216e8a43d9ae4a04f0
Matt Kuras <mattkuras@gmail.com>
2020-02-20 00:48:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,6 +5,7 @@ require 'directors_database'
 
 def directors_totals(nds)
 
+<<<<<<< HEAD
  result = {}
   row_index = 0 
   while row_index < nds.length do 
@@ -16,6 +17,7 @@ def directors_totals(nds)
     column_index = 0 
     
     while column_index < nds[row_index][:movies].length do  
+      
       result[name] += nds[row_index][:movies][column_index][:worldwide_gross]
       
       column_index += 1 
@@ -24,4 +26,21 @@ def directors_totals(nds)
     row_index += 1 
   end
 result 
+=======
+ one_director_total = 0 
+  row_index = 0 
+  while row_index < nds.length do 
+    
+    column_index = 0 
+    while column_index < nds[row_index][:movies].length do  
+            binding.pry 
+
+      one_director_total = one_director_total + nds[row_index][:movies][column_index][:worldwide_gross]
+      
+      column_index += 1 
+    end
+    one_director_total
+    row_index += 1 
+  end
+>>>>>>> 9caf49ec3bc48569df9ef0651e973e95737e6011
 end
